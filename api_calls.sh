7a25a7f14a14615539394e1c07812e5d3ff25c62
Daniele Torelli <me@mdtorelli.it>
2024-12-23 13:20:58 +0100

    Implement api calls

--- api_calls.sh
+++ api_calls.sh
@@ -8,9 +8,33 @@ function get_profile() {
   curl -Ss -H "Accept: application/json" "${GOLEM_API_HOST}/users/${1?}"
 }
 
+function follow() {
+  TARGET="${2?}"
+  curl -Ss -H "Accept: application/json" -X POST "${GOLEM_API_HOST}/users/${1?}/follows" -d "{\"user\":\"${TARGET}\"}"
+}
+
+function unfollow() {
+  TARGET="${2?}"
+  curl -Ss -H "Accept: application/json" -X DELETE "${GOLEM_API_HOST}/users/${1?}/follows" -d "{\"user\":\"${TARGET}\"}"
+}
+
+function tweet() {
+  CONTENT="${2?}"
+  curl -Ss -H "Accept: application/json" -X POST "${GOLEM_API_HOST}/users/${1?}/tweets" -d "{\"content\":\"${CONTENT}\"}"
+}
+
+function get_tweets() {
+  curl -Ss -H "Accept: application/json" "${GOLEM_API_HOST}/users/${1?}/tweets"
+}
+
+function get_timeline() {
+  curl -Ss -H "Accept: application/json" "${GOLEM_API_HOST}/users/${1?}/timeline"
+}
+
 function compare_json() {
-  ACTUAL=$(echo "${1?}" | jq -S)
-  EXPECTED=$(echo "${2?}" | jq -S)
+  JQ_REMOVE_TIMESTAMPS='  if .tweets then .tweets |= map(del(.timestamp)) elif .tweet then .tweet |= del(.timestamp) else . end'
+  ACTUAL=$(echo "${1?}" | jq -S ${JQ_REMOVE_TIMESTAMPS})
+  EXPECTED=$(echo "${2?}" | jq -S ${JQ_REMOVE_TIMESTAMPS})
 
   diff --color=always -u <(echo $ACTUAL) <(echo $EXPECTED) | (grep -v '^---' | grep -v '^+++' | grep -v '^@@' || true)
 }
@@ -24,6 +48,21 @@ if [ "${BASH_SOURCE[0]}" == "${0}" ]; then
   case ${1} in
     get_profile)
       get_profile "${2?}"
+      ;;
+    follow)
+      follow "${2?}" "${3?}"
+      ;;
+    unfollow)
+      unfollow "${2?}" "${3?}"
+      ;;
+    tweet)
+      tweet "${2?}" "${3?}"
+      ;;
+    get_tweets)
+      get_tweets "${2?}"
+      ;;
+    get_timeline)
+      get_timeline "${2?}"
       ;;
     *)
       echo "Invalid argument: ${1}"
